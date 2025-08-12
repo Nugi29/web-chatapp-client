ee88f11970c2c72cdb8242e868fb0930805defe5
Nugitha Disas <nugitha.c@gmail.com>
2025-08-13 00:03:17 +0530

    Implement user authentication functions in AuthContext; add login, logout, and updateProfile methods

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
@@ -34,7 +34,48 @@ export const AuthProvider = ({ children }) => {
     };
 
     //Login function to handle user authentication and socket connection
-    
+    const login = async (state, credentials) => {
+        try {
+            const { data } = await axios.post(`/api/auth/${state}`, credentials);
+            if (data.success) {
+                setAuthUser(data.userData);
+                connectSocket(data.userData);
+                axios.defaults.headers.common["token"] = data.token;
+                setToken(data.token);
+                localStorage.setItem("token", data.token);
+                toast.success(data.message);
+            } else {
+                toast.error(data.message);
+            }
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
+
+    //Logout function to handle user logout and socket disconnection
+    const logout = async () => {
+        localStorage.removeItem("token");
+        setToken(null);
+        setAuthUser(null);
+        setOnlineUser([]);
+        axios.defaults.headers.common["token"] = null;
+        toast.success("Logged out successfully");
+        socket.disconnect();
+    };
+
+    // update profile function to handle user profile updates
+    const updateProfile = async (body) => {
+        try {
+            const { data } = await axios.put("/api/auth/update-profile", body);
+            if (data.success) {
+                setAuthUser(data.user);
+                toast.success("Profile updated successfully");
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
 
     // Connect socket function to handle socket connection and online user update
     const connectSocket = (userData) => {
@@ -67,10 +108,11 @@ export const AuthProvider = ({ children }) => {
         axios,
         authUser,
         onlineUser,
-        socket
+        socket,
+        login,
+        logout,
+        updateProfile
     };
-
-
 
     return (
         <AuthContext.Provider value={value}>

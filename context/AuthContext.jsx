b06aa2900f19bac78196e076904085af3ac7eed7
Nugitha Disas <nugitha.c@gmail.com>
2025-08-12 21:58:34 +0530

    Implement AuthContext for user authentication and socket connection; add dependencies for axios, react-hot-toast, and socket.io-client

---
+++ context/AuthContext.jsx
@@ -0,0 +1,80 @@
+import { createContext } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { io } from "socket.io-client";
+
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL = backendUrl;
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+
+    const [token, setToken] = useState(localStorage.getItem("token"));
+    const [authUser, setAuthUser] = useState(null);
+    const [onlineUser, setOnlineUser] = useState(null);
+    const [socket, setSocket] = useState(null);
+
+    //Check if user is authenticated and if so set the user data  and connect to the socket
+    const checkAuth = async () => {
+        try {
+            const { data } = await axios.get("/api/auth/check");
+
+            if (data.success) {
+                setAuthUser(data.user);
+                connectSocket(data.user);
+            }
+
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+
+    };
+
+    //Login function to handle user authentication and socket connection
+    
+
+    // Connect socket function to handle socket connection and online user update
+    const connectSocket = (userData) => {
+        if (!userData || socket?.connected) return;
+
+        const newSocket = io(backendUrl, {
+            query: { userId: userData._id, }
+        });
+        newSocket.connect();
+        setSocket(newSocket);
+
+        newSocket.on("getOnlineUsers", (userIds) => {
+            setOnlineUser(userIds);
+        });
+
+
+
+    }
+
+
+    useEffect(() => {
+        if (token) {
+            axios.defaults.headers.common["token"] = token;
+            checkAuth();
+
+        }
+    }, []);
+
+    const value = {
+        axios,
+        authUser,
+        onlineUser,
+        socket
+    };
+
+
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
